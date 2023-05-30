4a9d8aa4fe20aa1e25dcc78421951a9513ecbaa0
Kauany <Kauany.lima@estudante.ifro.edu.br>
2023-05-30 15:29:00 -0400

    Tratamento de exceção

---
+++ aula2/divisao.cs
@@ -0,0 +1,19 @@
+﻿/*int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int divisao = a / b;
+Console.WriteLine(divisao);
+*/
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+
+    int divisao = a / b;
+    Console.WriteLine("o resultado é " + divisao);
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo");
+}
