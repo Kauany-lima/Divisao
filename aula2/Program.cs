1e460eb067360ff100630dd89ba148a531832336
Kauany <Kauany.lima@estudante.ifro.edu.br>
2023-05-30 15:03:14 -0400

    inicio do projeto de divisao de dois números

---
+++ aula2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+      /* Aluno aluno1 = new Aluno();
+        aluno1.nome = "Amanda";
+        aluno1.email = "email";
+       double primeiranota = Convert.ToDouble(Console.ReadLine());
+     double segundanota = Convert.ToDouble(Console.ReadLine());
+
+aluno1.CalcularMedia(8,9);
+
+       
+        Aluno aluno2 = new Aluno();
+        aluno2.nome = "Alice";
+        aluno2.email = "email";
+      
+
+        aluno1.CalcularMedia(5,3);
+      */
+
+
---
+++ aula2/exercicio.cs
@@ -0,0 +1,13 @@
+﻿public class exercicio
+{
+    public string nomeCliente;
+    public double poupança;
+    public double corrente;
+    public double saldo;
+
+
+}
+public class sacar
+{
+
+}
